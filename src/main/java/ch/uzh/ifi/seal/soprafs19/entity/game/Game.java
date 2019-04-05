d430ede3b81a2b256751e9f6b7616ac2cbca7de4
Gioele <ogimgamers@gmail.com>
2019-04-05 20:39:39 +0200

    starting game class

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/game/Game.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/game/Game.java
@@ -1,10 +1,44 @@
 package ch.uzh.ifi.seal.soprafs19.entity.game;
 
 import javax.persistence.Entity;
+import java.util.*;
+
+import ch.uzh.ifi.seal.soprafs19.entity.User;
+import ch.uzh.ifi.seal.soprafs19.entity.game.rules.GameRuleEngine;
+import ch.uzh.ifi.seal.soprafs19.entity.game.states.GameState;
 
 @Entity
 public class Game {
 
+    List<User> listOfUser = new ArrayList<User>();
+    public int idGame;
+    public String NameGame;
+    public GameRuleEngine gameruleEngine = new GameRuleEngine();
+
+    public Board board = new Board();
+    public GameState gameState = new GameState();
+
+    public boolean allowGodPowers(boolean answer){
+        if(answer == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void addUser(User user){
+        listOfUser.add(user);
+
+    }
+    public void removeUser(User user){
+        listOfUser.remove(user);
+
+    }
+    public void setGodPower(String godPower1, String godPower2){}
+
+    public void start(){
+        //Board.load();
+    }
 
 
 }
